327cb8bb550d8b76387dd2b683a928a7f27f7af7
jorge_rabello <jorge.rabello3@gmail.com>
2024-09-09 12:46:30 -0300

    feat: First Commit

---
+++ src/main/java/br/com/jorgerabellodev/jpark/JparkApplication.java
@@ -0,0 +1,13 @@
+package br.com.jorgerabellodev.jpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JparkApplication.class, args);
+	}
+
+}
